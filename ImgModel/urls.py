f6a6270ab5aac849d66d7cdf7f3ffcf6b49102a8
chunsikman <sb97213@naver.com>
2024-01-23 21:40:36 +0900

    배포 준비

--- ImgModel/urls.py
+++ ImgModel/urls.py
@@ -4,4 +4,5 @@ from ImgModel import views
 
 urlpatterns =[
     path('findFood', views.find_food),
+    path('findFood_for_kakao', views.find_food_for_kakao),
 ]
--- ImgModel/views.py
+++ ImgModel/views.py
@@ -8,6 +8,7 @@ import base64
 import logging
 from PIL import Image
 import io
+import requests
 
 # 로거 설정
 logger = logging.getLogger(__name__)
@@ -48,13 +49,13 @@ def find_food(request):
                     img_array = np.expand_dims(np.asarray(image_resize), axis=0)
                     predictions = model.predict(img_array)
                     # 상위 4개 인덱스 가져오기
-                    top_4_foods = [ int(e) for e in np.argsort(predictions[0])[-4:]]
+                    top_4_foods = [int(e) for e in np.argsort(predictions[0])[-4:]]
                     # 상위 4개 확률 가져오기
                     top_4_probabilities = [round(float(prob) * 100, 2) for prob in predictions[0][top_4_foods]]
 
                     # 각 이미지에 대한 예측 결과 생성
                     result = {
-                        'base64_encoded_data' : base64_encoded_data,
+                        'base64_encoded_data': base64_encoded_data,
                         'category': category[:category.find('[')],
                         'foodnum': top_4_foods[3],
                         'candidate1': top_4_foods[2],
@@ -78,3 +79,45 @@ def find_food(request):
             return JsonResponse({'status': 'error', 'message': 'No files processed'}, status=400)
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
+
+
+@csrf_exempt
+def find_food_for_kakao(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('userName', 'default_user')
+        meal_time = request.POST.get('meal_time')
+        images_urls = {k: v for k, v in request.POST.items() if k.startswith('image')}
+
+        results = []  # 각 이미지에 대한 예측 결과를 저장할 리스트
+
+        # 이미지 URL 데이터 처리
+        for category, image_url in images_urls.items():
+            response = requests.get(image_url)  # URL로부터 이미지를 가져옴
+            image = Image.open(io.BytesIO(response.content))  # 응답에서 이미지를 로드
+            image_resize = image.resize((300, 300))
+
+            # 음식 이름 예측
+            model = apps.ImgmodelConfig.model
+            img_array = np.expand_dims(np.asarray(image_resize), axis=0)
+            predictions = model.predict(img_array)
+            # 상위 4개 인덱스 가져오기
+            top_4_foods = [int(e) for e in np.argsort(predictions[0])[-4:]]
+            # 상위 4개 확률 가져오기
+            top_4_probabilities = [round(float(prob) * 100, 2) for prob in predictions[0][top_4_foods]]
+
+            # 각 이미지에 대한 예측 결과 생성
+            result = {
+                'base64_encoded_data': "null",
+                'category': category[:category.find('[')],
+                'foodnum': top_4_foods[3],
+                'candidate1': top_4_foods[2],
+                'candidate2': top_4_foods[1],
+                'candidate3': top_4_foods[0],
+                'predictrate': top_4_probabilities[3],
+                'candidate1rate': top_4_probabilities[2],
+                'candidate2rate': top_4_probabilities[1],
+                'candidate3rate': top_4_probabilities[0],
+            }
+            results.append(result)
+
+    return JsonResponse({'status': 'success', 'results': results})
